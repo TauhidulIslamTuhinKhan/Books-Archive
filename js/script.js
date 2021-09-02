177830551a7d42be7dc2489c8d3015eaad559d98
TauhidulIslamTuhinKhan <tuhin0411@gmail.com>
2021-09-02 18:03:00 +0600

    second

--- js/script.js
+++ js/script.js
@@ -1,54 +1,49 @@
-
 const errorDiv = document.getElementById('error-message');
 const bookNumber = document.getElementById('book-number');
 const searchField = document.getElementById('search-field');
 const searchResult = document.getElementById('search-result');
 const spinner = document.getElementById('spinner');
 
-// const toggleSearchResult = searchResult => {
-//     searchResult.style.display = searchResult;
-// }
 
 // search button
 const searchItem = () => {
     //clear text
-    bookNumber.innerHTML = ''; 
+    bookNumber.innerHTML = '';
     searchResult.textContent = '';
     const searchText = searchField.value;
-    if(searchText === ''){
+    if (searchText === '') {
         errorDiv.innerText = 'Search field cannot be empty'; //error message
-        return;       
+        return;
     };
     //clear text
     errorDiv.innerText = '';
     searchField.value = '';
     spinner.classList.remove('d-none'); // spinner add
     searchResult.classList.add('d-block'); // content add
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        if(data.numFound === 0) {
-            errorDiv.innerText = 'No Result Found'; //error message
-            spinner.classList.add('d-none'); // spinner remove
-            searchResult.classList.remove('d-block'); // content remove
-        }
-        else{
-            errorDiv.innerText = '';
-            displaySearchItem(data.docs)            
-        }
-    }); 
+        .then(res => res.json())
+        .then(data => {
+            if (data.numFound === 0) {
+                errorDiv.innerText = 'No Result Found'; //error message
+                spinner.classList.add('d-none'); // spinner remove
+                searchResult.classList.remove('d-block'); // content remove
+            } else {
+                errorDiv.innerText = '';
+                displaySearchItem(data.docs)
+            }
+        });
 };
 
 
 // display search item
 const displaySearchItem = (items) => {   
-    console.log(items);  
-      // show number of books after scearch  
+    // show number of result after scearch  
     bookNumber.innerHTML = `
-    <p>${items.length} book found</P>
-    `;      
+    <p>${items.length} Result found</P>
+    `;
     items.forEach(item => {
+        console.log(item);
         const div = document.createElement('div');
         div.classList.add('card');
         div.innerHTML = `        
